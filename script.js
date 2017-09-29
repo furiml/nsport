39f97287ffd4090db7f6f5d96d7b4d4b122d668a
Furiml <a.screve.94801@gmail.com>
2017-09-29 15:10:29 +0200

    calculator working

--- script.js
+++ script.js
@@ -6,30 +6,45 @@ const listes = {
 
 // prix des prestations
 const prices = {
+	default : 0,
 	cours_col_beginner_1 : 100,
 	cours_col_beginner_2 : 150,
 	presta1 : 10,
 	presta2 : 20
 }
 
+// initiation du sélecteur
 var selected = null;
 
+// création des listeners
 for (let liste in listes) {
-	//console.log(i);
-	//console.log(listes[i]);
 	const dropdown = document.getElementById(liste);
 	dropdown.addEventListener('change', handleChange);
 }
 
+// détection d'un changement, log console et calcul du prix
 function handleChange() {
 	selected = this.options[this.selectedIndex].value;
 	logPrice();
+	calculateTotal();
 }
 
+// log console du prix sélectionné
 function logPrice() {
 	if (selected in prices) {
 		console.log(prices[selected]);
 	} else {
 		console.log(0);
 	}
+}
+
+// calcul du total
+function calculateTotal() {
+	var total_price = 0;
+	for (let liste in listes) {
+		const dropdown = document.getElementById(liste);
+		total_price += prices[dropdown.options[dropdown.selectedIndex].value];
+	}
+	console.log(total_price);
+	document.getElementById('prix').innerHTML = total_price + '€';
 }
